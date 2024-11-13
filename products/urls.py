852999237bc4c288a330fb18ad66eb9b6f828ddd
EdgarQuinonez <l22410043@itparral.edu.mx>
2024-11-13 00:02:09 -0600

    Created first app products. Installed DRF. Connection established to postgres db using service file and passfile. Ready to test deploy

---
+++ products/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+from products.views import ProductViewSet
+
+
+router = routers.DefaultRouter()
+
+router.register('products', ProductViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- sakura_django/settings.py
+++ sakura_django/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'products',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'sakura_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'sakura_django',
+            'passfile': '.pgpass',            
+        },
     }
 }
 
